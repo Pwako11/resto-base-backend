23ebded324508ce9472addcc17f02861d9ef1ce3
Pwako11 <akolo@hotmail.com>
2022-02-13 15:44:04 -0500

    Updated users model to show association

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    has_many :products, foreign_key: :user_id
+    validates_uniqueness_of :email, :username
+    validates_presence_of :name, :username, :email, :password_digest
+end
